043f0f95d58c04183e42f6fbed360d954a828121
Beizbol <30479707+Beizbol@users.noreply.github.com>
2024-03-03 05:30:05 -0500

    upgrade to asgi

--- sln/py/hey/jv0/main.py
+++ sln/py/hey/jv0/main.py
@@ -1,11 +1,18 @@
-from bottle import route, run
+from blacksheep import Application, get
+import uvicorn as server
+
+app = Application()
 
 
-@route("/")
-@route("/hey")
-def hey():
+@get("/")
+async def home():
+    return "Hello, World!"
+
+
+@get("/hey")
+async def hey():
     return "Hey from Python!"
 
 
 if __name__ == "__main__":
-    run(host="localhost", port=4269)
+    server.run("main:app", port=4269, log_level="info")
---
+++ sln/py/hey/jv0/readme.md
@@ -0,0 +1,5 @@
+# run
+
+```
+$ .venv/bin/uvicorn server:app
+```
